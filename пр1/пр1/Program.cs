71002a4b9afbf1fd119a0848e0d7dbfa5e7610b7
VikaLiubchenko <113911211+VikaLiubchenko@users.noreply.github.com>
2023-04-24 16:58:38 +0300

    Add files via upload

---
+++ пр1/пр1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Любченко В.М., Факультет iнформацiйних технологiй, спецiальнiсть iнженерiя програмного забезпечення,форма навчання денна,група 7");
+            System.Console.ReadLine();
+        }
+        
+    }
+}
