7349b4e6bab9893cdfaa270982a1e45ec146797a
VikaLiubchenko <113911211+VikaLiubchenko@users.noreply.github.com>
2023-04-24 16:50:07 +0300

    Add files via upload

---
+++ пр5/пр5/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+class Program
+{ public static void Main()
+    { Console.WriteLine("Введіть два слова :");
+        var word1 = Console.ReadLine();
+        var word2 = Console.ReadLine(); 
+        var fullWord = word1 + word2; 
+        Console.WriteLine("Лише ті літери слів, які зустрічаються в обох словах лише один раз:"); 
+        for (int i = 0; i < fullWord.Length; i++)
+        { var c = fullWord[i].ToString();
+            if (fullWord.IndexOf(c) == fullWord.LastIndexOf(c))
+            { Console.WriteLine(c); 
+            }
+        } 
+    }
+}
